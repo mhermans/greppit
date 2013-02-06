36f3ea3285424f4f1864fba7f8d662ca26e8f0a1
Maarten Hermans <maarten.hermans@gmail.com>
2013-02-06 01:53:44 +0100

    start crawler mockup

--- crawler.py
+++ crawler.py
@@ -1,5 +1,22 @@
-import praw, pandas, datetime
-r = praw.Reddit(user_agent='test-please ignore || misbehaving?:/u/mhermans')
+import datetime
+import py2neo, pandas
+from mpatch import Reddit, Subreddit, Submission, Comment, Redditor
 
-def get_submissions(fetch_user=True, fetch_comments=False):
-    pass
+class RedditCrawler(object):
+    def __init__(self, reddit_user_name=None, reddit_password=None,
+            neo4j_uri="http://localhost:7474/db/data/"):
+        """docstring for __init__"""
+
+        self.gdb = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+        self.reddit_api = praw.Reddit(
+                user_agent='neo4reddit crawler || misbehaving?:/u/mhermans')
+
+    def crawl_subreddit(self, name, limit=100, full=True, resume=True):
+        """docstring for crawl_subreddits"""
+        pass
+
+    def clear_db(self):
+        """WARNING: clears the entire graph!"""
+
+        q = 'START n=node(*) MATCH n-[r?]-() where ID(n) <> 0 DELETE n, r;'
+        py2neo.cypher.execute(self.gdb, q)
