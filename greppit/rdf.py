02e00e8088e1951369e9b3f2ac5c47beb81fc019
Maarten Hermans <maarten.hermans@gmail.com>
2013-06-02 13:29:07 +0200

    add rdf crawl function to submission object

--- greppit/rdf.py
+++ greppit/rdf.py
@@ -1,21 +1,24 @@
-import rdflib, datetime
-from rdflib import URIRef, Literal, RDF, RDFS
-from greppit import RedditGraph, Submission, Comment
+import datetime
+from rdflib import URIRef, Literal, RDF, RDFS, Namespace, Graph
+from greppit import RedditGraph, Submission, Comment, Subreddit
 from lxml import html
 import HTMLParser
 
-SIOC = rdflib.Namespace('http://rdfs.org/sioc/ns#')
-SIOCT = rdflib.Namespace('http://rdfs.org/sioc/types#')
-DCT = rdflib.Namespace('http://purl.org/dc/terms/')
-DC = rdflib.Namespace('http://purl.org/dc/elements/1.1/')
-DBP = rdflib.Namespace('http://dbpedia.org/resource/')
-XTYPES = rdflib.Namespace('http://purl.org/xtypes/')
-BIBO = rdflib.Namespace('http://purl.org/ontology/bibo/')
-RLDO = rdflib.Namespace('http://purl.org/net/rldo/')
+SIOC = Namespace('http://rdfs.org/sioc/ns#')
+SIOCT = Namespace('http://rdfs.org/sioc/types#')
+DCT = Namespace('http://purl.org/dc/terms/')
+DC = Namespace('http://purl.org/dc/elements/1.1/')
+DBP = Namespace('http://dbpedia.org/resource/')
+XTYPES = Namespace('http://purl.org/xtypes/')
+BIBO = Namespace('http://purl.org/ontology/bibo/')
+RLDO = Namespace('http://purl.org/net/rldo/')
 
-def _init_graph():
+def _init_graph(identifier=None):
     # TODO available on Reddit objects?
-    g = rdflib.Graph()
+
+    if identifier: g = Graph(identifier=identifier)
+    else: g = Graph()
+
     g.namespace_manager.bind("sioc", SIOC)
     g.namespace_manager.bind("sioct", SIOCT)
     g.namespace_manager.bind('dcterms', DCT)
@@ -26,6 +29,36 @@ def _init_graph():
     g.namespace_manager.bind('rldo', RLDO)
 
     return g
+
+
+
+# TODO: parse (cached) from http://www.reddit.com/r/semanticweb/wiki/meta/prefixes
+PREFIXES = """
+@prefix air: <http://www.daml.org/2001/10/html/airport-ont#> .
+@prefix cert: <http://www.w3.org/ns/auth/cert#> .
+@prefix contact: <http://www.w3.org/2000/10/swap/pim/contact#> .
+@prefix dc: <http://purl.org/dc/elements/1.1/> .
+@prefix dct: <http://purl.org/dc/terms/> .
+@prefix doap: <http://usefulinc.com/ns/doap#> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
+@prefix mvcb: <http://webns.net/mvcb/> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix rel: <http://purl.org/vocab/relationship/> .
+@prefix rss: <http://purl.org/rss/1.0/> .
+@prefix session: <http://redfoot.net/2005/session#> .
+@prefix sioc: <http://rdfs.org/sioc/ns#> .
+@prefix sioct: <http://rdfs.org/sioc/types#> .
+@prefix uranai: <http://kota.s12.xrea.com/vocab/uranai#> .
+@prefix wn: <http://xmlns.com/wordnet/1.6/> .
+"""
+
+
+# ================== #
+#     SUBMISSION     #
+# ================== #
 
 def _get_submission_graph(self):
 
@@ -54,10 +87,11 @@ def _get_submission_graph(self):
         Literal(datetime.datetime.fromtimestamp(d.get('created_utc')))))
 
 
+    sr_uri = ''.join(['http://www.reddit.com', self.subreddit.url])
+    g.add((post_uri, SIOC.has_container, URIRef(sr_uri)))
+
 
     # id <-> name (includes type)
-
-    # TODO containing subreddit?
 
     # no accepted voting properties atm.
     # http://wiki.sioc-project.org/index.php/Ontology/RatingTermsSuggestion
@@ -69,12 +103,16 @@ def _get_submission_graph(self):
 
     if d.get('author_name'):
         user_uri = URIRef('/'.join(['http://www.reddit.com/user', d['author_name']]))
-        g.add((user_uri, RDF.type, SIOC.UserAccount))
         g.add((post_uri, SIOC.has_creator, user_uri))
 
     return g
 
 Submission.graph = _get_submission_graph
+
+
+# =============== #
+#     COMMENT     #
+# =============== #
 
 def _get_comment_graph(self):
     d = self.data()
@@ -127,28 +165,6 @@ def _contains_rdf(self):
     # fast detection
     return ':::turtle' in self.body
 
-# TODO: parse (cached) from http://www.reddit.com/r/semanticweb/wiki/meta/prefixes
-PREFIXES = """
-@prefix air: <http://www.daml.org/2001/10/html/airport-ont#> .
-@prefix cert: <http://www.w3.org/ns/auth/cert#> .
-@prefix contact: <http://www.w3.org/2000/10/swap/pim/contact#> .
-@prefix dc: <http://purl.org/dc/elements/1.1/> .
-@prefix dct: <http://purl.org/dc/terms/> .
-@prefix doap: <http://usefulinc.com/ns/doap#> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
-@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
-@prefix mvcb: <http://webns.net/mvcb/> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix rel: <http://purl.org/vocab/relationship/> .
-@prefix rss: <http://purl.org/rss/1.0/> .
-@prefix session: <http://redfoot.net/2005/session#> .
-@prefix sioc: <http://rdfs.org/sioc/ns#> .
-@prefix sioct: <http://rdfs.org/sioc/types#> .
-@prefix uranai: <http://kota.s12.xrea.com/vocab/uranai#> .
-@prefix wn: <http://xmlns.com/wordnet/1.6/> .
-"""
 
 def _parse_turtle_block(self):
     if not self._contains_rdf():
@@ -173,16 +189,28 @@ def _parse_turtle_block(self):
 
     return g
 
+
+#TODO generic walk function for comments
+
 Comment.graph = _get_comment_graph
 Comment._links = _links
 Comment._contains_rdf = _contains_rdf
 Comment._parse_turtle_block = _parse_turtle_block
 
 
-#TODO generic walk function for comments
 
-# SUBREDDIT
-# ---------
+
+# ================ #
+#     REDDITOR     #
+# ================ #
+
+
+
+        #g.add((user_uri, RDF.type, SIOC.UserAccount)) # add in User-function
+
+# ================= #
+#     SUBREDDIT     #
+# ================= #
 
 def _get_subreddit_graph(self):
     g = _init_graph()
@@ -190,7 +218,17 @@ def _get_subreddit_graph(self):
     g.add((sr_uri, RDF.type, SIOC.Forum))
     g.add((sr_uri, SIOC.topic, DBP.Semantic_web))
 
+def _crawl_subreddit(self, limit=100, full=True, resume=True):
+    gg = Graph()
+    for i, subm in enumerate(self.get_hot(limit=limit)):
+        #log.info('Parsing submission nr.%s' % i)
+        print subm
+        gg += subm.graph()
+
+    return gg
+
+Subreddit.crawl = _crawl_subreddit
+
 r = RedditGraph('test')
-#s = r.get_submission('http://www.reddit.com/r/dataisbeautiful/comments/18e617/the_word_tumultuous/')
 s = r.get_submission('http://www.reddit.com/r/semanticweb/comments/197py9/w3c_turtle_now_a_candidate_recommendation/')
 c = s.comments[0]
